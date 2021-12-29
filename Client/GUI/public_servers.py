66273f86e875f72422987d01ae8705ec491fa348
adam-sadeg <rawritsadam90@gmail.com>
2021-12-29 23:20:20 +0000

    Starting to get the project off the ground. This commit is mostly some gui work with 2 renames. Nothing special.

---
+++ Client/GUI/public_servers.py
@@ -0,0 +1,6 @@
+from tkinter import Frame
+
+class PublicServersFrame(Frame):
+    def __init__(self):
+        super().__init__()
+        
---
+++ Client/GUI/root_window.py
@@ -0,0 +1,23 @@
+from tkinter import Tk, ttk
+from public_servers import PublicServersFrame
+
+class RootWindow(Tk):
+    def __init__(self, w:int, h:int):
+        super().__init__()
+        self.w = w
+        self.h = h
+        self.__setup_tk_config()
+        self.tab_controller = ttk.Notebook(self)
+        self.tab_controller.pack(expand=1, fill="both")
+
+    def __setup_tk_config(self):
+        self.resizable(width=False, height=False)
+        self.geometry(f"{self.w}x{self.h}")
+
+    def add_new_tab(self, tab_frame, name:str):
+        self.tab_controller.add(tab_frame, text=name)
+
+    def get_tab_controller(self):
+        return self.tab_controller
+
+RootWindow(1200, 800).mainloop()
