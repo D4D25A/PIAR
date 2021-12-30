1a16c69a29f9112ccd8450f6c0c880c6cfa11d4c
ayple <rawritsadam90@gmail.com>
2021-12-30 16:53:07 +0000

    test

--- Server/server.py
+++ Server/server.py
@@ -4,6 +4,12 @@ The commands will be from sent in the form of new_command_to_server({'cmd':COMMA
 the client and the server will send back new_command_to_client({'cmd':COMMAND_ID, 'data': RES_DATA}) 
 and vice versa.
 """
+
+from client_handler import Client
+from room_handler import *
+
 class Server:
-    def __init__(self):
-        pass
+    def __init__(self, listen_ip, listen_port):
+        self.listen_ip = listen_ip
+        self.listen_port = listen_port
+        
